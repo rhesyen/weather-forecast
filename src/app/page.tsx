a025bc2b34a4d6cf2425613fd9517f2f773257b5
Aniela Fendrych <afendrych@mp.pl>
2025-03-07 13:55:15 +0100

    deleting unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-import LikeButton from "./LikeButton";
-
-function Header({ title }: { title: string }) {
-  return <h1>{title ? title : "Default title"}</h1>;
-}
-
 export default function HomePage() {
-  const names = ["First name", "Second name", "Third name"];
-
-  return (
-    <div>
-      <Header title="Hello React with TypeScript" />
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-      <LikeButton />
-    </div>
-  );
+  return <div></div>;
 }
