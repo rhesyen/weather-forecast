d6db9116bda812b5b701365bede37d120a5f3d05
Aniela Fendrych <afendrych@mp.pl>
2025-04-18 13:35:51 +0200

    updating SearchWeather.tsx with new functionality

--- src/components/SearchWeather.tsx
+++ src/components/SearchWeather.tsx
@@ -1,7 +1,18 @@
-const SearchWeather = (props: any) => {
+interface SearchWeatherProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchWeather = ({ value, onChange }: SearchWeatherProps) => {
   return (
     <div className="search-weather">
-      Szukaj: <input type="text" {...props} />
+      Szukaj:{" "}
+      <input
+        className="p-1 border rounded-md m-2"
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
